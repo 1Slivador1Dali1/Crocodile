6bee11f7a74b45485b9d31b86dbd390e5340180f
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-12 15:12:13 +0300

    add js code

--- crocodile.js
+++ crocodile.js
@@ -1,14 +1,34 @@
 const listWord = ["Пузырь", "Телепузики", "Морская свинка", "Выхухоль", "Скунс", "Сумасшедшая белка", "Сиреневенький носок", "Золотой унитаз", "Чупакабра"];
 
+function insertRandomStringFromArray(listWord, selector) {
+    let element = document.querySelector(selector);
 
-function ger() {
-    document.getElementsByClassName('.button').addEventListener("click", generateWord);
-    function generateWord() {
-        const randomIndex = Math.floor(Math.random() * listWord.length);
-        const wordElement = document.getElementsByTagName('p');
-        wordElement.textContent = listWord[randomIndex];
-    };
+    if (element) {
+        element.textContent = listWord[Math.floor(Math.random() * listWord.length)];
+    }
+    else {
+        console.error('Элемент с данным селектором не найден');
+    }
 }
 
-generateWord();
-ger();
+function handleButtonClick() {
+    let strings = listWord;
+    let selector = '#word-text';
+    insertRandomStringFromArray(strings, selector);
+}
+
+let buttonGenerate = document.querySelector('button');
+buttonGenerate.addEventListener('click', handleButtonClick);
+
+
+// function ger() {
+//     document.getElementsByClassName('.button').addEventListener("click", generateWord);
+//     function generateWord() {
+//         const randomIndex = Math.floor(Math.random() * listWord.length);
+//         const wordElement = document.getElementsByTagName('p');
+//         wordElement.textContent = listWord[randomIndex];
+//     };
+// }
+
+// generateWord();
+// ger();
